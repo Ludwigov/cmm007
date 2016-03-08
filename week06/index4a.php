b04dc45a35be4fc3debad38a1d1df9e051a6a0a8
1405114 <a@a.com>
2016-03-08 10:54:06 +0000

    1

---
+++ week06/index4a.php
@@ -0,0 +1,15 @@
+<?php
+$myage=20;
+if($myage >= 21) {
+    print "specs";
+}
+elseif($myage >=18){
+    print "mugs";
+}
+elseif($myage >=16){
+    print "sausage rolls";
+}
+else{
+    print "Thank you for your visit";
+}
+?>
