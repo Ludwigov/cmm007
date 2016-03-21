28c2d6716d2958a345a224c23f27d55470c223f0
1405114 <a@a.com>
2016-03-21 11:22:59 +0000

    1

---
+++ aszu.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+
+<h1>Helloooojgvujoooohfcyfdygoooo</h1>
+
+</body>
+</html>
