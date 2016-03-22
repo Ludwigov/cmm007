f75c9f5c678bc5eaffa95a7c4a7fa43f025bf553
1405114 <a@a.com>
2016-03-22 11:18:19 +0000

    1

--- GetnPost/personsname.php
+++ GetnPost/personsname.php
@@ -1,7 +1,7 @@
 
 
 <form action="formResponse.php" method="post">
- <label>Persons name</label><input type="text" name ="Persons name">;
+ <label>Persons name</label><input type="text" name ="forename">;
  <label>Gender</label><input type="text" name ="Gender">;
  <label>Main Superhero Power</label><input type="text" name ="Main Superhero Power">;
     <input type="submit" value="Submit">
