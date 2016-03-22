3e1d2beeed1b1d873a27517639ee5f3304d5ca99
1405114 <a@a.com>
2016-03-22 10:09:14 +0000

    1

---
+++ GetnPost/personsname.php
@@ -0,0 +1,7 @@
+
+<form action="formResponse.php" metyhod="post">
+ <label>Persons name</label><input type="text" name ="Persons name">;
+ <label>Gender</label><input type="text" name ="Gender">;
+ <label>Main Superhero Power</label><input type="text" name ="Main Superhero Power">;
+    <input type="submit" value="Submit">
+    </form>
---
+++ GetnPost/superheresponse.php
@@ -0,0 +1,6 @@
+<?php
+$Persons name = $_POST["Persons name"];
+$"Gender" =$_POST["Gender"];
+ echo $Persons name . $Gender;
+ echo $Main Superhero Power;
+?>
