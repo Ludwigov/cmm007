f97cc45856b97d8c9daac885917beafbb87a56fc
1405114 <a@a.com>
2016-03-22 11:28:49 +0000

    1

--- GetnPost/formResponse.php
+++ GetnPost/formResponse.php
@@ -1,5 +1,8 @@
 <?php
 $forename = $_POST['forename'];
 $surname = $_POST['surname'];
+$power = $_POST['mainpower'];
 echo "The name is {$forename} {$surname}";
+echo "<br>";
+echo "My main super power is {$power}";
 ?>
--- GetnPost/personsname.php
+++ GetnPost/personsname.php
@@ -2,7 +2,8 @@
 
 <form action="formResponse.php" method="post">
  <label>Persons name</label><input type="text" name ="forename">;
- <label>Gender</label><input type="text" name ="Gender">;
- <label>Main Superhero Power</label><input type="text" name ="Main Superhero Power">;
+    <label>Last Name</label><input type="text" name ="surname">;
+ <label>Gender</label><input type="text" name ="gender">;
+ <label>Main Superhero Power</label><input type="text" name ="mainpower">;
     <input type="submit" value="Submit">
     </form>
