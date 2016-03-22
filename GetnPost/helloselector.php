e9f96546af67381a75164290397d89ac816d32f7
1405114 <a@a.com>
2016-03-22 10:28:15 +0000

    1

---
+++ GetnPost/helloselector.php
@@ -0,0 +1,22 @@
+<?php
+  $planet = $_GET['planet'];
+ echo "planet is".$Mars;
+
+?>
+
+<html>
+<head>
+    <h1>Where would you like to say hello?</h1>
+</head>
+
+<body>
+<a href="http://1405114.azurewebsite.net/GetnPost/helloPrinter.php?planet=Earth">Earth</a>"
+<a href="http://1405114.azurewebsite.net/GetnPost/helloPrinter.php?planet=Mars">Mars</a>
+<a href="http://1405114.azurewebsite.net/GetnPost/helloPrinter.php?planet=Uranus">Uranus</a>
+<main>
+
+    <p>Hello <?php echo $p; ?></p>
+
+</main>
+</body>
+</html>
