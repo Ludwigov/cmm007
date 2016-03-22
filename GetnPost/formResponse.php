4991a05cc5d3443f922b44767ca6ec9256ba8391
1405114 <a@a.com>
2016-03-22 10:44:09 +0000

    1

--- GetnPost/formResponse.php
+++ GetnPost/formResponse.php
@@ -1,5 +1,5 @@
 <?php
-$forename = $_GET['forename'];
-$surname = $_GET['surname'];
+$forename = $_POST['forename'];
+$surname = $_POST['surname'];
 echo "The name is {$forename} {$surname}";
 ?>
--- GetnPost/post.php
+++ GetnPost/post.php
@@ -1,9 +1,8 @@
-
 <html>
-    <head>
-     </head>
-<form action="formResponse.php" method="GET">
-<label>Forename</label><input type="text" name="forename">
+<head>
+</head>
+<form action="formResponse.php" method="POST">
+    <label>Forename</label><input type="text" name="forename">
     <label>Surname</label><input type="text" name="surname">
     <input type="submit" value="Submit">
-    </form>
+</form>
