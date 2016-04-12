089eab6f41a0d9a692b3735f3ac84787e06fdc13
1405114 <a@a.com>
2016-04-12 10:23:09 +0100

    1

--- april2016/checksession.php
+++
@@ -1,9 +0,0 @@
-<?php
-///CHECKING SESSION DATA
-
-  session_start();
-
-  if (isset($_SESSION['FirstName']))  {
-      /**  */your code here
-    }
-  ?>
---
+++ april2016/loggedin.php
@@ -0,0 +1,18 @@
+<?php
+///CHECKING SESSION DATA
+
+  session_start();
+  $accesslevel =$_COOKIE['access_Level_cookie'];
+
+    displayAccessLevelInformation($accessLevel);
+
+    function displayAccessLevelInformation($accessLevel)
+    }
+     if ($accessLevel =="standarduser")  {
+         echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user </p>";
+    }
+     elseif ($accessLevel == "root") {
+         echo "<p<p style = \\";
+         echo "<p<p style = \\";
+     }
+  ?>
