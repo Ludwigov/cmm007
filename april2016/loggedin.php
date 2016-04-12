e9f031f330fe336cc3345a186496d808d53d219a
1405114 <a@a.com>
2016-04-12 10:57:27 +0100

    1

--- april2016/loggedin.php
+++ april2016/loggedin.php
@@ -8,16 +8,15 @@ echo "aaa";
   session_start();
   $accesslevel =$_COOKIE['access_Level_cookie'];
 
-/*   displayAccessLevelInformation($accessLevel);
+ displayAccessLevelInformation($accessLevel);
 
    function displayAccessLevelInformation($accessLevel)
    {
-    if ($accessLevel =="standarduser")  {
-        echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user </p>";
+       if ($accessLevel == "standarduser") {
+           echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user </p>";
+       } elseif ($accessLevel == "root") {
+           echo "<p<p style = \"background-color: red\">You are currently logged as a root user</p>";
+           echo "<p<p style = \"background-color: red\">You now have access to additional administrative features</p>";
+       }
    }
-    elseif ($accessLevel == "root") {
-        echo "<p<p style = \"background-color: red\">You are currently logged as a root user</p>";
-        echo "<p<p style = \"background-color: red\">You now have access to additional administrative features</p>";
-    }
-*/
   ?>
