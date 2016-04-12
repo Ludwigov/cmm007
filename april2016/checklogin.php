e1eb4b47d8ade896da0df643e263add684d8270b
1405114 <a@a.com>
2016-04-12 11:08:34 +0100

    1

--- april2016/checklogin.php
+++ april2016/checklogin.php
@@ -1,12 +1,14 @@
 <?php
      $username = $_POST["username"];
- $password = $_POST["password"];
+     $password = $_POST["password"];
 
 if ($username =="Ludov" && $password=="goodluckludov")
 {
+    session_start();
     setcookie('access_level_cookie','standarduser');
-}
     header('Location: loggedin.php');
+}
+
 
 ?>
 
