4cfa841a30096bc0c6059d980fc317773bc6f5c6
1405114 <a@a.com>
2016-04-14 12:40:45 +0100

    1

--- april2016/checksession.php
+++ april2016/checksession.php
@@ -6,9 +6,9 @@
 
     function displayAccessLevelInformation($accessLevel)
     {
-    if ($accessLevel == "standarduser") {
+    if ($access_Level == "standarduser") {
         echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user </p>";
-    } elseif ($accessLevel == "root") {
+    } elseif ($access_Level == "root") {
         echo "<p<p style = \"background-color: red\">You are currently logged as a root user</p>";
         echo "<p<p style = \"background-color: red\">You now have access to additional administrative features</p>";
     }
