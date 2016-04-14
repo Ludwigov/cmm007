f0c8ca95cd464bd49fae5de0da5791e2f7b60506
1405114 <a@a.com>
2016-04-14 19:30:14 +0100

    1

--- april2016/printPOST.php
+++ april2016/printPOST.php
@@ -1,5 +1,10 @@
 <?php
 
     if(isset($_POST['submit'])) {
+        $thelist = $_POST['thelist'];
+        $lastname = $_POST[''];
+        $addname = $_POST['theinput'];
         echo "Now are you are here";
+        echo $addname . "<br>";
+        echo $thelist;
     }
