a203684a0f6f322ad55d2e8d7ca7e84dafb0fa96
1405114 <a@a.com>
2016-04-19 17:20:16 +0100

    1

---
+++ marvelstory/dbconnect.php
@@ -0,0 +1,7 @@
+  <?php
+  define('DB_SERVER', 'your-azure-server');
+  define('DB_USERNAME', 'your-username');
+  define('DB_PASSWORD', 'your-username');
+  define('DB_DATABASE', 'your-username');
+  $db =mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+  ?>
---
+++ marvelstory/home.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+      <meta charset="utf-8">
+      <title>Welcome to Marvelmovies</title>
+      <link rel="stylesheet" href="style.css" />
+</head>
+   <p>What would you like to do?</p>
+ <ul>
+     <li><a href="insertmarvel.....php">Insert ......</a></li>
+     <li><a href="insertmarvel.....php">Insert ......</a></li>
+     <li><a href="insertmarvel.....php">Insert ......</a></li>
+     <li><a href="insertmarvel.....php">Insert ......</a></li>
+     <li><a href="insertmarvel.....php">Insert ......</a></li>
+
+  </ul>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
