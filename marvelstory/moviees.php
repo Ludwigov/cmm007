77caae91eb17c8bfe3b281923a8fef36d4097fed
1405114 <a@a.com>
2016-04-21 19:11:12 +0100

    1

--- marvelstory/moviees.php
+++ marvelstory/moviees.php
@@ -6,11 +6,9 @@
  * Time: 18:10
  */
 include("dbconnect.php");
-          $movies=$_GET['Allmovies'];
-          $marvel=$_GET['Marvelmovies'];
-          $after=$_GET['Moviesafter2010'];
-          $Xmen=$_GET['Xmenmovies'];
-      if($movies){
+          $rest=$_GET['moviees'];
+        switch($rest) {
+            case 'Allmovies':
           $sql="SELECT * FROM marvelmovies ";
           $result=$db->query($sql);
           while($row=$result->fetch_array()){
@@ -21,11 +19,12 @@ include("dbconnect.php");
               $notes=$row['notes'];
 
               echo "{$marvelMovieID}.{$yearReleased}.{$title}.{$productionStudio}.{$notes}";
+
           }
+                break;
 
-      }
 
-      if($marvel){
+          case 'Marvelmovies':
           $sql="SELECT * FROM marvelmovies WHERE productionStudio = 'marvelMovies'";
           $result=$db->query($sql);
           while($row=$result->fetch_array()){
@@ -36,9 +35,11 @@ include("dbconnect.php");
               $notes=$row['notes'];
 
               echo "{$marvelMovieID}.{$yearReleased}.{$title}.{$productionStudio}.{$notes}";
+
           }
-      }
-      if($after){
+              break;
+
+          case 'Moviesafter2010':
           $sql="SELECT * FROM marvelmovies WHERE yearReleased>'2010'";
           $result=$db->query($sql);
           while($row=$result->fetch_array()){
@@ -49,9 +50,11 @@ include("dbconnect.php");
               $notes=$row['notes'];
 
               echo "{$marvelMovieID}.{$yearReleased}.{$title}.{$productionStudio}.{$notes}";
+
           }
-      }
-       if($Xmen) {
+              break;
+
+            case'Xmenmovies':
            $sql = "SELECT * FROM marvelmovies WHERE  title LIKE 'X-men'";
            $result = $db->query($sql);
            while ($row = $result->fetch_array()) {
@@ -62,7 +65,9 @@ include("dbconnect.php");
                $notes = $row['notes'];
 
                echo "{$marvelMovieID}.{$yearReleased}.{$title}.{$productionStudio}.{$notes}";
+               break;
            }
+       }
 //           ////***
 //             $sql = "INSERT INTO movies () VALUES() ";
 //             $sql = "INSERT INTO marvel () VALUES() ";
@@ -70,5 +75,5 @@ include("dbconnect.php");
 //             $sql = "INSERT INTO Xmen () VALUES() ";
 //              ***////
 //       }
-       }
+
 
