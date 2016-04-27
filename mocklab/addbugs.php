18ad81346c080edeee3cfd5c741e395cf0d83555
1405114 <a@a.com>
2016-04-27 21:31:58 +0100

    1

--- mocklab/addbugs.php
+++ mocklab/addbugs.php
@@ -1,7 +1,66 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1405114
- * Date: 26/04/2016
- * Time: 17:15
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example</title>
+    <link rel="stylesheet" type ="text/css"  href="addbugs.css">
+</head>
+<body>
+
+<fieldset>
+    <header>
+        <img id="logo" src="images/logo.png">
+        <div id="titles">
+            <h1>BugTracker</h1>
+            <h2>Keeping track of all pesky little bugs</h2>
+        </div>
+    </header>
+</fieldset>
+
+    <main>
+       <fieldset>
+         <nav>
+            <ul>
+                <li><a href="showbugs.html">All Bug Items</a></li><br>
+                <li><a href="showbugs.html">Android Bugs</a></li><br>
+                <li><a href="showbugs.html">iOS Bugs</a></li><br>
+                <li><a href="showbugs.html">Windows Bugs</a></li><br>
+                <li><a href="addbugs.html">Insert Bug</a></li><br>
+            </ul>
+          </nav>
+        </fieldset>
+
+     </main>
+
+      include ("connect.php");
+      if(isset($_GET['category'])) {
+          $bugsCategory + $_GET['Category'];
+          echo "category: ($bugscetegory) <hr>";
+          $sql = "SELECT = FROM bugs WHERE cetegory= '$bugsCategory'";
+      }
+      else {
+          echo "category: all bugs <hr>";
+          $sql = "SELECT*FROM bugs";
+      }
+          $result = $db->query($sql);
+          while($row =$result->fetch_array()) {
+              $name = $row['name'];
+              $summary = $row['summary'];
+              $category = $row['category'];
+              echo "<li>{$name} {$summary} {$category}</li>";
+          }
+
+           <form>
+              <table><p>
+                  <tr><td > Name:</td ><td ><input type = 'text' class='form2' name = 'link'
+                  <tr><td > Summary:</td ><td ><textarea class='form2' cols = '101' rows = '5'
+                  <tr><td > Category:</td ><td ><input type = 'text' class='form2' name = 'category'
+                  </table ></p >
+          </form ><br >
+
+              }
+   ?>
+
+
+
+
--- mocklab/showbugs.php
+++ mocklab/showbugs.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,10 +7,29 @@
 </head>
 <body>
 <form action="submitform.php" method ="post">
-    <input type="text" placeholder="Bug" name="Bug">
-    <input type="text" placeholder=""    name="">
-    <input type="text" placeholder=""    name="">
-    <input type="submit" value =""  >
+    <input type="text" placeholder="First Name" name="firstname">
+    <input type="text" placeholder="Last Name"    name="lastname">
+    <input type="submit" value ="Please Press Enter"  >
 </form>
 </body>
 </html>
+?>
+
+
+
+
+//include database
+include ("db_connect.php");
+
+$name = $_POST["name"];
+$summary = $_POST["summary"];
+$category = $_POST["category"];
+
+$sql = "INSERT INTO bugs (name,summary,category) VALUES ('$name','$summary','$category')"";
+
+        if (mysqli_query($db, $sql)){
+        } else{
+            echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        }
+        header("location:index.php");
+?>
