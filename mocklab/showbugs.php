f21b8c6da52bafd6d6b4bde8c79f46ca6dcd3a68
1405114 <a@a.com>
2016-04-27 17:37:15 +0100

    1

--- mocklab/showbugs.php
+++ mocklab/showbugs.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1405114
- * Date: 26/04/2016
- * Time: 17:15
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My form<</title>
+</head>
+<body>
+<form action="submitform.php" method ="post">
+    <input type="text" placeholder="Bug" name="Bug">
+    <input type="text" placeholder=""    name="">
+    <input type="text" placeholder=""    name="">
+    <input type="submit" value =""  >
+</form>
+</body>
+</html>
