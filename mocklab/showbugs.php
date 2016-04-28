dee54f9f2830939aca45e48691502f94442965ad
1405114 <a@a.com>
2016-04-28 13:08:41 +0100

    1

--- mocklab/showbugs.php
+++ mocklab/showbugs.php
@@ -11,25 +11,23 @@
     <input type="text" placeholder="Last Name"    name="lastname">
     <input type="submit" value ="Please Press Enter"  >
 </form>
-</body>
-</html>
 
 
+     //include database
+   include ("db_connect.php");
 
+   $name = $_POST["name"];
+   $summary = $_POST["summary"];
+  $category = $_POST["category"];
 
-
-//include database
-include ("db_connect.php");
-
-$name = $_POST["name"];
-$summary = $_POST["summary"];
-$category = $_POST["category"];
-
-$sql = "INSERT INTO bugs (name,summary,category) VALUES ('$name','$summary','$category')"";
+  $sql = "INSERT INTO bugs (name,summary,category) VALUES ('$name','$summary','$category')"";
 
         if (mysqli_query($db, $sql)){
         } else{
             echo "Error: " . $sql . "<br>" . mysqli_error($db);
         }
         header("location:index.php");
-?>
+   ?>
+
+</body>
+</html>
