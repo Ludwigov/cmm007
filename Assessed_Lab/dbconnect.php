11acffe9ef5af527c9d816d553950ecdb3e5defd
1405114 <a@a.com>
2016-05-04 12:13:06 +0100

    1

---
+++ Assessed_Lab/dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b419ceeda1d055');
+define('DB_PASSWORD', '5b8fbe3f');
+define('DB_DATABASE', 'CMM007ALDB-1405114');
+$db =mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
